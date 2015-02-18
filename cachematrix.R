e4c6dcc00feac9fc64ca5b1f24600d6f3b8ed3e4
unknown <m-enudi@CO-IT-MICHAEL.leadway.com.ng>
2015-02-18 19:51:47 +0100

    committing my assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+##  
+## The functions in the file provide functionality that can create a matrix which
+## which caches its inverse (assuming that it has one) such that without changing
+## without changing the values of the matrix, a call to cacheSolve which lookup
+## a previously calculated inverse rather than recalculate
+##
 ## Write a short comment describing this function
 
+## makeCacheMatrix creates and encapsulates a internal matrix and returns a set
+## list of operations or variables of that list that are
+## themselves functions to lookup the interval matrix
 makeCacheMatrix <- function(x = matrix()) {
     #store a local copy of the matrix cache and its dimension
     minv <- NULL
@@ -31,7 +39,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 ## Write a short comment describing this function
-
+## cacheSolves accepts a data structure from the function
+## makeCacheMatrix as x and returns the inverse of it.
+## it first checks to see if there already exist an inverse 
+## If such inverse exist, it returns it else it calculates the
+## the inverse of the matrixs and assigns it back to x
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     inv <- x$getInverse()
